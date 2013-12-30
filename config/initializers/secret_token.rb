ddbbaa6f515d2b4dca42c230d050eeac37871cbc
TomShacham <shachamtom@gmail.com>
2013-12-30 15:16:26 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letmegolf::Application.config.secret_key_base = 'cb19958a502365fce6f7b62f83a82fd39cece93810700783400748005a1e1840228183afd3a7a8cec1e6949d3ab1bae6cd8789d6e8657635a43dcbe3cc3bb328'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Letmegolf::Application.config.session_store :cookie_store, key: '_letmegolf_session'
