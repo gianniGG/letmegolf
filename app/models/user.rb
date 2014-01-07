5ac7a22b4eacbb4f6a3d88e0185106cee136ca0a
TomShacham <shachamtom@gmail.com>
2014-01-07 18:17:33 +0000

    insert sign in architecture

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   # before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true,
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   has_secure_password
 
